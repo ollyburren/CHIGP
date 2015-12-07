fbe97d7e02c568f6e8818fb3a6cb17f351858362
ollyburren <oliver.burren@cimr.cam.ac.uk>
2015-12-07 14:23:50 +0000

    bash fixes

--- sh/test_ppi.sh
+++ sh/test_ppi.sh
@@ -7,9 +7,6 @@ if [ -z "$RSCRIPT_BIN" ];
 	RSCRIPT_BIN=`which Rscript`;
 fi
 
-echo "$TABIX_BIN"
-
-
 if [ -z "$TABIX_BIN" ];
   then
 	TABIX_BIN=`which tabix`
@@ -27,6 +24,7 @@ if [ ! -x "$TABIX_BIN"  ];
 	echo "Please make sure tabix is installed and set TABIX_BIN env variable";
 	exit 1;
 fi
+
 echo "$RSCRIPT_BIN ../R/computePPi.R --kg_compress_dir=../DATA/1kgenome/VCF/EUR/by.chr.phase3/ALL. --region_file=../DATA/support/0.1cM_chr22 --gwas_tbx=../DATA/gwas/OKADA_RA_22.bed.gz --gwas_type=QUANT --n_samples=58293 --prop_cases=0.25 --kg_compress_suffix=.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.EUR.vcf.gz --tabix_bin=$TABIX_BIN --pi_i=0.0001 --out_dir=../DATA/out/\n"
 
 $RSCRIPT_BIN ../R/computePPi.R --kg_compress_dir=../DATA/1kgenome/VCF/EUR/by.chr.phase3/ALL. --region_file=../DATA/support/0.1cM_chr22 --gwas_tbx=../DATA/gwas/OKADA_RA_22.bed.gz --gwas_type=QUANT --n_samples=58293 --prop_cases=0.25 --kg_compress_suffix=.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.EUR.vcf.gz --tabix_bin=$TABIX_BIN --pi_i=0.0001 --out_dir=../DATA/out/
